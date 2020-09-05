d882e5b85fe2437feba76f1bdbd26c0fd86b7cb6
mynamaneet <70784280+mynamaneet@users.noreply.github.com>
2020-09-04 19:26:34 -0700

    Initial commit

---
+++ src/main/java/mynamaneet/examplemod/ExampleMod.java
@@ -0,0 +1,31 @@
+package mynamaneet.examplemod;
+
+import java.util.ArrayList;
+
+import com.mynamaneet.dolmodloader.*;
+import com.mynamaneet.dolmodloader.exceptions.InvalidLocationException;
+import com.mynamaneet.dolmodloader.exceptions.InvalidPassageException;
+import com.mynamaneet.dolmodloader.exceptions.InvalidTweeFileException;
+
+
+public class ExampleMod extends Mod {
+    public ExampleMod(){
+        super("Example Mod", "1.0.5", "mynamaneet");
+    }
+
+    public static void main() {
+        //Handled by ModLoader
+    }
+
+	@Override
+	public void modApp() {
+        try{
+            ArrayList<String> message = new ArrayList<>();
+            message.add("ExampleMod Test");
+            message.add("ExampleMod 2nd Test");
+            ModLoader.addPassageText(message, ModLoader.getDolPassage(ModLoader.getTweeFile(ModLoader.getDolLocation("loc-home"), "main"), "Bedroom"));
+        }catch(InvalidPassageException|InvalidLocationException|InvalidTweeFileException ex){
+            ModLoader.logMessage(ex.getMessage());
+        }
+	}
+}
