a401308592a187123b0f39445a64736aa8305c26
mynamaneet <70784280+mynamaneet@users.noreply.github.com>
2020-09-23 20:17:51 -0700

    Testing 0.1.19

--- src/main/java/mynamaneet/examplemod/ExampleMod.java
+++ src/main/java/mynamaneet/examplemod/ExampleMod.java
@@ -7,28 +7,29 @@ import com.mynamaneet.dolmodloader.exceptions.InvalidLocationException;
 import com.mynamaneet.dolmodloader.exceptions.InvalidPassageException;
 import com.mynamaneet.dolmodloader.exceptions.InvalidTweeFileException;
 
-
 public class ExampleMod extends Mod {
-    public ExampleMod(){
+    public ExampleMod() {
         super("Example Mod", "1.0.7", "mynamaneet");
     }
 
     public static void main() {
-        //Handled by ModLoader
+        // Handled by ModLoader
     }
 
-	@Override
-	public void modApp() {
-        try{
+    @Override
+    public void modApp() {
+        try {
             ArrayList<String> message = new ArrayList<>();
             message.add("ExampleMod Test");
             message.add("<br>");
             message.add("ExampleMod 2nd Test");
-            ModLoader.addPassageText(message, ModLoader.getDolPassage(ModLoader.getTweeFile(ModLoader.getDolLocation("loc-home"), "main"), "Bedroom"));
+            ModLoader.addPassageText(message, ModLoader
+                    .getDolPassage(ModLoader.getTweeFile(ModLoader.getDolLocation("loc-home"), "main"), "Bedroom"));
             message.clear();
             message.add("Urmomgay");
-            ModLoader.addPassageText(message, ModLoader.getDolPassage(ModLoader.getTweeFile(ModLoader.getDolLocation("loc-home"), "main"), "Bedroom"));
-        }catch(InvalidPassageException|InvalidLocationException|InvalidTweeFileException ex){
+            ModLoader.addPassageText(message, ModLoader
+                    .getDolPassage(ModLoader.getTweeFile(ModLoader.getDolLocation("loc-home"), "main"), "Bedroom"));
+        } catch (InvalidPassageException | InvalidLocationException | InvalidTweeFileException ex) {
             ModLoader.logMessage(ex.getMessage());
         }
 	}
