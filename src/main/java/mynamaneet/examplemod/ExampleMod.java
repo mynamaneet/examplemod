f4d9c52b60e44a9f1726c57886425c8932e5d712
mynamaneet <70784280+mynamaneet@users.noreply.github.com>
2020-09-30 21:25:50 -0700

    Testing 0.1.19

--- src/main/java/mynamaneet/examplemod/ExampleMod.java
+++ src/main/java/mynamaneet/examplemod/ExampleMod.java
@@ -1,11 +1,14 @@
 package mynamaneet.examplemod;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.mynamaneet.dolmodloader.*;
 import com.mynamaneet.dolmodloader.exceptions.InvalidLocationException;
 import com.mynamaneet.dolmodloader.exceptions.InvalidPassageException;
 import com.mynamaneet.dolmodloader.exceptions.InvalidTweeFileException;
+import com.mynamaneet.dolmodloader.file_classes.DolPassage;
 
 public class ExampleMod extends Mod {
     public ExampleMod() {
@@ -19,16 +22,31 @@ public class ExampleMod extends Mod {
     @Override
     public void modApp() {
         try {
+            DolPassage bedroom = ModLoader.getDolPassage(
+                ModLoader.getTweeFile(
+                    ModLoader.getDolLocation(
+                        "loc-home"), 
+                    "main"), 
+            "Bedroom");
+
             ArrayList<String> message = new ArrayList<>();
             message.add("ExampleMod Test");
             message.add("<br>");
             message.add("ExampleMod 2nd Test");
-            ModLoader.addPassageText(message, ModLoader
-                    .getDolPassage(ModLoader.getTweeFile(ModLoader.getDolLocation("loc-home"), "main"), "Bedroom"));
+            ModLoader.addPassageText(message, bedroom);
+
             message.clear();
-            message.add("Urmomgay");
-            ModLoader.addPassageText(message, ModLoader
-                    .getDolPassage(ModLoader.getTweeFile(ModLoader.getDolLocation("loc-home"), "main"), "Bedroom"));
+
+            // BufferedReader r = new BufferedReader(ModLoader.getResource(this, "important.txt"));
+            // String line;
+            // while((line=r.readLine()) != null){
+            //     message.add(line);
+            // }
+            // r.close();
+
+            message = ModLoader.getResource(this, "important.txt");
+
+            ModLoader.addPassageText(message, bedroom);
         } catch (InvalidPassageException | InvalidLocationException | InvalidTweeFileException ex) {
             ModLoader.logMessage(ex.getMessage());
         }
