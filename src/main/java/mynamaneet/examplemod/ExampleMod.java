55cc9f2651c052d8e790f90580abe3a107531c7a
mynamaneet <70784280+mynamaneet@users.noreply.github.com>
2020-10-04 19:12:23 -0700

    Testing 0.1.20-dev

--- src/main/java/mynamaneet/examplemod/ExampleMod.java
+++ src/main/java/mynamaneet/examplemod/ExampleMod.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import com.mynamaneet.dolmodloader.*;
 import com.mynamaneet.dolmodloader.exceptions.InvalidLocationException;
 import com.mynamaneet.dolmodloader.exceptions.InvalidPassageException;
+import com.mynamaneet.dolmodloader.exceptions.InvalidSubfolderException;
 import com.mynamaneet.dolmodloader.exceptions.InvalidTweeFileException;
+import com.mynamaneet.dolmodloader.file_classes.DolLocation;
 import com.mynamaneet.dolmodloader.file_classes.DolPassage;
+import com.mynamaneet.dolmodloader.file_classes.DolSubfolder;
+import com.mynamaneet.dolmodloader.file_classes.TweeFile;
 
 public class ExampleMod extends Mod {
     public ExampleMod() {
@@ -20,17 +24,17 @@ public class ExampleMod extends Mod {
     @Override
     public void modApp() {
         try {
-            DolPassage bedroom = ModLoader.getDolPassage(
-                ModLoader.getTweeFile(
-                    ModLoader.getDolLocation(
-                        "loc-home"), 
-                    "main"), 
-            "Bedroom");
+            DolSubfolder subfolder = ModLoader.getDolSubfolder("overworld-town");
+            DolLocation location = ModLoader.getDolLocation("loc-cafe");
+            TweeFile twee = ModLoader.getTweeFile(location, "chef.twee");
+            DolPassage passage = ModLoader.getDolPassage("Bedroom");
 
-            ArrayList<String> message = ModLoader.getTextResource(this, "important.txt");
-            ModLoader.overwritePassage(bedroom, message);
-            
-        } catch (InvalidPassageException | InvalidLocationException | InvalidTweeFileException ex) {
+            ModLoader.logMessage("SUBFOLDER - "+subfolder.getDirectoryPath().getAbsolutePath());
+            ModLoader.logMessage("LOCATION - "+location.getDirectoryPath().getAbsolutePath());
+            ModLoader.logMessage("TWEE - "+twee.getDirectoryPath().getAbsolutePath());
+            ModLoader.logMessage("PASSAGE - "+passage.getTweeFile().getAbsolutePath());
+
+        } catch (InvalidLocationException | InvalidTweeFileException | InvalidSubfolderException | InvalidPassageException ex) {
             ModLoader.logMessage(ex.getMessage());
         }
 	}
